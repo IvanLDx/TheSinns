800562b4a7fe59283f49ce13b22ddf04243d790e
Iván Hermo <ratzi.dx@gmail.com>
2024-12-03 20:02:08 +0100

    Fix import

--- client/js/Socket/LoginSocket.js
+++ client/js/Socket/LoginSocket.js
@@ -2,7 +2,7 @@ const socket = window.socket;
 import { ifis } from '../utils.js';
 import { Modal } from '../models/components/Modal/Modal.js';
 import { BurgerButton } from '../models/components/BurgerMenu/BurgerButton.js';
-import { documentListeners } from '../helpers/documentListeners.js';
+import * as documentListeners from '../helpers/documentListeners.js';
 
 export class LoginSocket {
 	constructor() {
